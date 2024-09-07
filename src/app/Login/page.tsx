122bf13273690c73f29491c1b9f5802753e8b2aa
Tanh2003 <nhoxtuananh092@gmail.com>
2024-09-07 17:24:15 +0700

    Complete Form Login + Register + Responsive

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -5,10 +5,28 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
 import { blue } from '@mui/material/colors';
 import { red } from '@mui/material/colors';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import "@/styles/Login.css";
+import Link from 'next/link';
+const useWindowWidth = () => {
+    const [windowWidth, setWindowWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => setWindowWidth(window.innerWidth);
+
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }
+    }, []);
+
+    return windowWidth;
+};
 const Login = () => {
+    const windowWidth = useWindowWidth();
     useEffect(() => {
         const signUpButton = document.getElementById('signUp');
         const signInButton = document.getElementById('signIn');
@@ -44,31 +62,41 @@ const Login = () => {
                 <div className="form-container sign-up-container">
                     <form action="#">
                         <h1>Tạo tài khoản</h1>
-                        <div className="social-container">
-                            <a href="https://www.facebook.com/FreeWebsiteCode/" className="social"><i className=""></i><FacebookOutlinedIcon sx={{ color: blue[500] }}/></a>
-                            <a href="https://twitter.com/freewebsitecode" className="social"><i className="fab fa-twitter"></i><GitHubIcon /></a>
-                            <a href="https://www.linkedin.com/in/freewebsitecode/" className="social"><i className="fab fa-linkedin-in"></i><GoogleIcon sx={{ color: red[500] }}/></a>
-                        </div>
-                        <span>hoặc sử dụng email để đăng ký</span>
                         <input type="text" placeholder="Họ và tên" />
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Mật khẩu" />
+                         <input type="repassword" placeholder="Nhập lại mật khẩu" />
+                        <span className='name-or'>Hoặc</span>
+                         <div className="social-container">
+                            <a href="#" className="social"><i className=""></i><FacebookOutlinedIcon sx={{ color: blue[500] }}/></a>
+                            <a href="#" className="social"><i className="fab fa-twitter"></i><GitHubIcon /></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i><GoogleIcon sx={{ color: red[500] }}/></a>
+                        </div>
                         <button>Đăng ký</button>
                     </form>
                 </div>
                 <div className="form-container sign-in-container">
                     <form action="#">
                         <h1>Đăng nhập</h1>
-                        <div className="social-container">
-                         <a href="https://www.facebook.com/FreeWebsiteCode/" className="social"><i className=""></i><FacebookOutlinedIcon sx={{ color: blue[500] }}/></a>
-                                                       <a href="https://twitter.com/freewebsitecode" className="social"><i className="fab fa-twitter"></i><GitHubIcon /></a>
-                            <a href="https://www.linkedin.com/in/freewebsitecode/" className="social"><i className="fab fa-linkedin-in"></i><GoogleIcon sx={{ color: red[500] }}/></a>
-                        </div>
-                        <span>hoặc sử dụng tài khoản của bạn</span>
+                        
+                       
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Mật khẩu" />
-                        <a href="#">Bạn quên mật khẩu?</a>
+                        <div className='forgot-password'>Bạn quên mật khẩu?</div>
+                         <span className='name-or'>Hoặc</span>
+                         <div className="social-container">
+                        <a href="#" className="social"><i className=""></i><FacebookOutlinedIcon sx={{ color: blue[500] }}/></a>
+                            <a href="#" className="social"><i className="fab fa-twitter"></i><GitHubIcon /></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i><GoogleIcon sx={{ color: red[500] }}/></a>
+                        </div>
                         <button>Đăng nhập</button>
+
+                        <div className="is-register" style={{ display: windowWidth < 768 ? 'block' : 'none' }}>
+                            Bạn chưa có tài khoản? <span ><a className='is-register-link' href="/Register">Đăng ký</a> </span>
+                            ngay!
+
+                        </div>
+                        
                     </form>
                 </div>
                 <div className="overlay-container">
@@ -79,8 +107,8 @@ const Login = () => {
                             <button className="ghost" id="signIn">Đăng nhập</button>
                         </div>
                         <div className="overlay-panel overlay-right">
-                            <h1>Xin chào,Bạn!</h1>
-                            <p>Nhập thông tin cá nhân của bạn và bắt đầu cuộc hành trình với chúng tôi</p>
+                            <h1>Chào mừng đến với của hàng </h1>
+                            <p>Khám phá những món đồ nội thất sang trọng, hiện đại, và tinh tế, giúp biến không gian sống của bạn thành tổ ấm hoàn hảo.</p>
                             <button className="ghost" id="signUp">Đăng ký</button>
                         </div>
                     </div>
