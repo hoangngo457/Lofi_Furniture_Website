a7797ccf7a3f8ba60227560f656b1771b62e4a24
Xuân Hoàng <hoangngo457@gmail.com>
2024-09-13 19:05:17 +0700

    fix router

--- src/app/Layout/component/menu_item.tsx
+++ src/app/Layout/component/menu_item.tsx
@@ -1,9 +1,9 @@
 export const menuItems = [
   { label: "Trang chủ", path: "/" },
-  { label: "Giới thiệu", path: "/" },
-  { label: "Sản phẩm", path: "/" },
-  { label: "Tin tức", path: "/" },
-  { label: "Liên hệ", path: "/" },
-  { label: "Câu hỏi thường gặp", path: "/" },
-  { label: "Hệ thống", path: "/" },
+  { label: "Giới thiệu", path: "/Introduce" },
+  { label: "Sản phẩm", path: "/Products" },
+  { label: "Tin tức", path: "/News" },
+  { label: "Liên hệ", path: "/Contact" },
+  { label: "Câu hỏi thường gặp", path: "/Question" },
+  { label: "Hệ thống", path: "/System" },
 ];
